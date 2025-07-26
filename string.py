c64fdc6028be28e0a193ab7efa9cb1b11db7a217
Loq13Julyx <loq13july@gmail.com>
2025-07-26 19:45:25 +0700

    String

---
+++ string.py
@@ -0,0 +1,10 @@
+nama = "Alief Chandra Darmawan"
+kota = 'Jember'
+alamat = 'Jawa Timur'
+
+nama_depan = "Alief"
+nama_tengah = "Chandra"
+nama_belakang = "Darmawan"
+nama_lengkap = nama_depan + " " +nama_tengah + " " +nama_belakang
+
+print(nama_lengkap)
