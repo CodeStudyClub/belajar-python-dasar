5b366bd4062eb651f00ef79966ad34b066ad5ee9
Loq13Julyx <loq13july@gmail.com>
2025-07-28 07:49:29 +0700

    If

---
+++ if.py
@@ -0,0 +1,11 @@
+orang = 100
+zomblie = 100
+
+if orang < zomblie:
+    print("Kabur, dunia mau kiamat!")
+    
+if orang > zomblie:
+    print("Ayo lawan, kita pasti menang!")
+    
+if orang == zomblie:
+    print("Ayo berunding, kita bisa hidup berdampingan!")
