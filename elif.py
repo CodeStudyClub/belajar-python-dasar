e85c1a21bb41d7ef7a9b7105dfb9769bbb28de41
Loq13Julyx <loq13july@gmail.com>
2025-07-28 08:04:42 +0700

    Elif

---
+++ elif.py
@@ -0,0 +1,14 @@
+menu_piliahan = input("Masukkan pilihan (1-3): ")
+
+if menu_piliahan == "1":
+    print("Anda memilih menu 1")
+    
+elif menu_piliahan == "2":
+    print("Anda memilih menu 2")
+
+elif menu_piliahan == "3":
+    print("Anda memilih menu 3")
+    
+else:
+    print("Pilihan tidak valid, silakan pilih antara 1-3")
+    
