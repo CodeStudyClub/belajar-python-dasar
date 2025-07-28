b12b7193b9394f532e5f6a4f6cc54f2b9da060c7
Loq13Julyx <loq13july@gmail.com>
2025-07-28 19:31:15 +0700

    For Loop

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+pelanggan = ["Alief", "Chandra", "Darmawan"]
+
+pelanggan.append("Loq13") 
+for nama in pelanggan:
+    print("=========================")
+    print("Nama Pelanggan: " + nama)    
+    print("=========================")
+    print("Terima kasih telah berbelanja di toko kami!")
