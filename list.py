fb9eefd40568fe1d985c02ec6c4ed25e4bb2860b
Loq13Julyx <loq13july@gmail.com>
2025-07-28 08:38:13 +0700

    List

---
+++ list.py
@@ -0,0 +1,15 @@
+nama = ["Alief", "Chandra", "Darmawan"]
+nama.append("Loq13")
+
+print(nama[0])
+print(nama[1])
+print(nama[2])
+print(nama[3])
+
+print(len(nama))
+
+nama.remove("Loq13")
+print(nama)
+
+nama[0] = "Loq13"
+print(nama)
