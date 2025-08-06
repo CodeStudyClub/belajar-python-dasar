908ef3d78f62bfea1bc9f7231c99de4547fc7eb5
Loq13Julyx <loq13july@gmail.com>
2025-08-06 08:18:17 +0700

    Method

---
+++ method.py
@@ -0,0 +1,15 @@
+nama = []
+
+def print_nama():
+    print("===============")
+    for data in nama:
+        print(data)
+
+nama.append("Alief")
+print_nama()
+
+nama.append("Chandra")
+print_nama()
+
+nama.append("Darmawan")
+print_nama()
