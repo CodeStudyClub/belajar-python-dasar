9ea872ac18fd3acce62dbcfbdf94c070f1ca6d98
Loq13Julyx <loq13july@gmail.com>
2025-08-06 08:34:04 +0700

    Default Parameter

---
+++ default_parameter.py
@@ -0,0 +1,6 @@
+def say_hello(first_name, last_name="User"):
+    print(f"Hello, {first_name} {last_name}!")
+
+say_hello("Alief", "Chandra")
+say_hello("Loq13")
+say_hello(last_name="Chandra", first_name="Alief")  
