6a3bbe92eca95ae9f18d8d4f358aede15ccb33b4
Loq13Julyx <loq13july@gmail.com>
2025-08-14 07:59:36 +0700

    Module

---
+++ function.py
@@ -0,0 +1,9 @@
+def saya_hello(nama):
+    return f"Hello {nama}"
+    
+def total(*list_angka):
+    hasil = 0
+    for data in list_angka:
+        hasil += data
+    return hasil
+
---
+++ module.py
@@ -0,0 +1,7 @@
+from function import saya_hello, total
+
+hello = saya_hello("Chandra")
+print(hello)
+
+total = total(1, 2, 3, 4, 5)
+print(total)
